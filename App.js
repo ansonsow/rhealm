b6ccc2150d6a3bf9c818f0da8e2a5922aea1f6d8
Marina Faria <marina.acfaria@gmail.com>
2023-06-16 01:08:10 -0700

    refactor: App used for screen testing

--- App.js
+++ App.js
@@ -2,19 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // import { useState } from 'react'
 // import axios from 'axios';
-import { SignUpContainer } from './src/components/containers/SignUpContainer';
 import { NativeBaseProvider } from 'native-base';
+import { SignUpContainer } from './src/components/containers/SignUpContainer';
+import { LoginContainer } from './src/components/containers/LoginContainer';
+import { ForgotPswContainer } from './src/components/containers/ForgotPswContainer';
+import { FirstScreen } from './src/components/screens/FirstScreen';
+import { InstructionContainer } from './src/components/containers/InstructionContainer';
+import { OnboardingOne } from './src/components/containers/OnboardingOne';
 
 export default function App() {
   // const [wa, setWa] = useState("")
   // axios.get("http://localhost:8000/api/v1/test").then((response) => {
   //   setWa(response.data[0].name)
   // })
+
   return (
     <NativeBaseProvider>
       <View style={styles.container}>
         {/* <Text>{wa}</Text> */}
-        <SignUpContainer />
+        {/* <SignUpContainer /> */}
+        <LoginContainer />
+        {/* <ForgotPswContainer /> */}
+        {/* <FirstScreen /> */}
+        {/* <InstructionContainer /> */}
+        {/* <OnboardingOne /> */}
         <StatusBar style="auto" />
       </View>
     </NativeBaseProvider>
