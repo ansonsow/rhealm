e30cec1dcf26555ab584c9bdba608779b1f8ead4
Marina Faria <marina.acfaria@gmail.com>
2023-06-20 23:44:38 -0700

    style: adjustments in styling

--- src/components/containers/InstructionContainer.js
+++ src/components/containers/InstructionContainer.js
@@ -1,13 +1,10 @@
 import { Text, Button, Container } from "native-base"
+import { StyleSheet } from "react-native"
 
 export const InstructionContainer = () => {
 
     const onNext = () => {
         console.log("Next Working")
-    }
-
-    const backToSplash = () => {
-        console.log("Splash Screen Working")
     }
 
     const inputSkinTone = () => {
@@ -17,51 +14,57 @@ export const InstructionContainer = () => {
     return (
         <Container>
             <Text
-                fontWeight="bold"
-                fontSize={20}
+                style={styles.heading}
             >
                 Instruction
             </Text>
             <Text
-                fontWeight="bold"
-                fontSize={16}
-                marginTop={10}
+                style={styles.subheading}
             >
                 In order to recommend based on your skin tone, we need your skin colour palette.
             </Text>
             <Text
-                fontSize={14}
-                marginTop={10}
+                style={styles.text}
             >
                 1. Please keep the distance of your camera about 20cm.
             </Text>
             <Text
-                fontSize={14}
-                marginTop={5}
+                style={styles.text}
             >
                 2. Please take a photo of your neck or arm.
             </Text>
             <Button
                 onPress={onNext}
-                marginTop={20}
-                width={250}
+                style={styles.btn}
             >
                 Next
             </Button>
             <Button
-                onPress={backToSplash}
-                marginTop={5}
-                width={250}
-            >
-                Back
-            </Button>
-            <Button
                 onPress={inputSkinTone}
-                marginTop={5}
-                width={250}
+                style={styles.btn}
             >
                 I know my skin tone already!
             </Button>
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    heading: {
+        fontWeight: "bold",
+        fontSize: 20
+    },
+    subheading: {
+        fontWeight: "bold",
+        fontSize: 16,
+        marginTop: 10
+    },
+    text: {
+        fontSize: 14,
+        marginTop: 10
+    },
+    btn: {
+        marginTop: 10,
+        width: 250
+    }
+})
