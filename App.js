e5147221cf07430395c5208a38e6227c191d463c
Marina Faria <marina.acfaria@gmail.com>
2023-06-20 23:38:59 -0700

    feat: testing different screens

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // import { useState } from 'react'
 // import axios from 'axios';
+import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
 import { SignUpContainer } from './src/components/containers/SignUpContainer';
 import { LoginContainer } from './src/components/containers/LoginContainer';
 import { ForgotPswContainer } from './src/components/containers/ForgotPswContainer';
-import { FirstScreen } from './src/components/screens/FirstScreen';
+import { WelcomeScreen } from './src/components/screens/WelcomeScreen';
 import { InstructionContainer } from './src/components/containers/InstructionContainer';
 import { OnboardingOne } from './src/components/containers/OnboardingOne';
+import { AppStack } from './src/components/stacks/AppStack';
+import { OnboardingTwo } from './src/components/containers/OnboardingTwo';
+import { Heading } from './src/components/layout/Heading';
+import { MainContainer } from './src/components/containers/MainContainer';
+import { ProfileContainer } from './src/components/containers/ProfileContainer';
 
 export default function App() {
   // const [wa, setWa] = useState("")
@@ -18,16 +24,23 @@ export default function App() {
 
   return (
     <NativeBaseProvider>
-      <View style={styles.container}>
-        {/* <Text>{wa}</Text> */}
-        {/* <SignUpContainer /> */}
-        <LoginContainer />
-        {/* <ForgotPswContainer /> */}
-        {/* <FirstScreen /> */}
-        {/* <InstructionContainer /> */}
-        {/* <OnboardingOne /> */}
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          {/* <Text>{wa}</Text> */}
+          {/* <SignUpContainer /> */}
+          {/* <LoginContainer /> */}
+          {/* <ForgotPswContainer /> */}
+          {/* <WelcomeScreen /> */}
+          {/* <InstructionContainer /> */}
+          {/* <OnboardingOne /> */}
+          {/* <AppStack /> */}
+          {/* <OnboardingTwo /> */}
+          {/* <Heading /> */}
+          {/* <MainContainer /> */}
+          <ProfileContainer />
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
