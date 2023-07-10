beb673e3f748834ee1d24c6c0b5b3fb1b407c131
Marina Faria <marina.acfaria@gmail.com>
2023-07-10 16:50:44 -0700

    RHL - 7 fix: forgot password page
    This page was styled, created error and success messages, but it's a dummy. It doesn't send a message to the user to update password.

--- src/components/forms/ForgotPswForm.js
+++ src/components/forms/ForgotPswForm.js
@@ -1,21 +1,37 @@
-import { Container, VStack, FormControl, HStack, Input, Button, Pressable, Text } from "native-base";
+import { Container, VStack, FormControl, HStack, Input, Button, Pressable, Text, View } from "native-base";
 import { useState } from "react";
+import { SvgXml } from "react-native-svg";
+import { svgAlertIcon, svgConfirmIcon } from "../../../assets/images/svgs";
+import { StyleSheet } from "react-native";
 
 export const ForgotPswForm = props => {
 
-    const { onSubmit, onNameChange, onEmailChange, error, login } = props;
+    const { onSubmit, onNameChange, onEmailChange, error, login, success } = props;
 
     return (
         <Container>
-            <VStack width="100%">
+            <VStack>
+                {error &&
+                    <View style={styles.alert}>
+                        <SvgXml
+                            xml={svgAlertIcon}
+                        />
+                        <Text color="#942100">{error}</Text>
+                    </View>}
+                {success &&
+                    <View style={styles.success}>
+                        <SvgXml
+                            xml={svgConfirmIcon}
+                            // fill="currentColor"
+                            style={styles.svg}
+                        />
+                        <Text color="#0c6e01">{success}</Text>
+                    </View>}
                 <FormControl isRequired>
                     <FormControl.Label>Name</FormControl.Label>
-                    <HStack width="100%">
+                    <HStack>
                         <Input
-                            placeholder="John Doe"
-                            width="100%"
-                            px={3}
-                            marginBottom={5}
+                            placeholder="Name"
                             onChangeText={value => {
                                 onNameChange(value)
                             }}
@@ -24,34 +40,67 @@ export const ForgotPswForm = props => {
                 </FormControl>
                 <FormControl isRequired>
                     <FormControl.Label>Email</FormControl.Label>
-                    <HStack width="100%">
+                    <HStack>
                         <Input
-                            placeholder="johndoe@gmail.com"
-                            width="100%"
-                            px={3}
-                            marginBottom={5}
+                            placeholder="Email"
                             onChangeText={value => {
                                 onEmailChange(value)
                             }}
                         />
                     </HStack>
                 </FormControl>
-                <Text color="red.500">{error}</Text>
-                <Button
+                <Pressable
                     onPress={onSubmit}
-                    marginTop={10}
-                    width={250}
                 >
-                    Reset Password
-                </Button>
-                <Button
+                    <Text style={styles.btnText}>Reset Password</Text>
+                </Pressable>
+                <Pressable
                     onPress={login}
-                    marginTop={5}
-                    width={250}
                 >
-                    Back to Login Page
-                </Button>
+                    <Text style={styles.btnText}>Back to Login Page</Text>
+                </Pressable>
             </VStack>
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    // ALERTS
+    alert: {
+        display: "flex",
+        flexDirection: "row",
+        gap: 10,
+        backgroundColor: "#f7dbd2",
+        padding: 10,
+        marginBottom: 10,
+        // width: "100%"
+        borderRadius: "10px",
+        alignContent: "center",
+        alignItems: "center"
+    },
+    success: {
+        display: "flex",
+        flexDirection: "row",
+        gap: 10,
+        backgroundColor: "#95cf8f",
+        padding: 10,
+        marginBottom: 10,
+        maxWidth: "100%",
+        borderRadius: "10px",
+        alignContent: "center",
+        alignItems: "center",
+    },
+
+    // BTN TEXT
+    btnText: {
+        color: "#fff",
+        textAlign: "center",
+        fontWeight: "bold"
+        // fontFamily: "indivisible-semibold"
+    },
+
+    // SVG - this is necessary to change considering every SVG we need to alter
+    svg: {
+        color: "#0c6e01"
+    },
+})
