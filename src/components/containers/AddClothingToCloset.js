606f934d3ef1e4014c3b6f5d93cf21ddab220c51
Marina Faria <marina.acfaria@gmail.com>
2023-07-14 10:19:05 -0700

    refactor: changes related to indentation
    This is done by one of my extensions

--- src/components/containers/AddClothingToCloset.js
+++ src/components/containers/AddClothingToCloset.js
@@ -20,177 +20,177 @@ import axios from "axios"
 
 export const AddClothingToCloset = () => {
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const route = useRoute();
-    const { closet } = route.params;
-    const [user, setUser] = useState('');
-    const [haveData, setHaveData] = useState(false)
-    const [clothings, setClothings] = useState([])
-    const [update, setUpdate] = useState(false)
-    const [selectedItems, setSelectedItems] = useState([])
+  const route = useRoute();
+  const { closet } = route.params;
+  const [user, setUser] = useState('');
+  const [haveData, setHaveData] = useState(false)
+  const [clothings, setClothings] = useState([])
+  const [update, setUpdate] = useState(false)
+  const [selectedItems, setSelectedItems] = useState([])
 
 
-    const getData = async () => {
-        try {
-            const jsonValue = await AsyncStorage.getItem('user');
-            if (jsonValue != null) {
-                setUser(JSON.parse(jsonValue).data)
-                getClothingData()
-            }
-        } catch (e) {
-            // error reading value
-        }
-    };
-
-    const getClothingData = () => {
-        // console.log(user._id)
-        axios.get(`${BACKEND}/clothing/user/${user._id}`).then(
-            (res) => {
-
-                setClothings(res.data.data)
-                // console.log(clothings)
-
-            }
-        ).catch(
-            (err) => {
-                console.log(err)
-            }
-        )
-
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('user');
+      if (jsonValue != null) {
+        setUser(JSON.parse(jsonValue).data)
+        getClothingData()
+      }
+    } catch (e) {
+      // error reading value
     }
+  };
 
-    const getClosetData = () => {
-        axios.get(`${BACKEND}/clothing/closet/${closet._id}`).then(
-            (res)=>{
-                // console.log(res.data)
-                // let tempArr = selectedItems;
-                // res.data.map((item,index)=>{
-                //     if(!tempArr.includes(item)){
-                //         tempArr.push(item)
-                //     }
-                // })
-                let tempArr = selectedItems.slice(); // Create a copy of selectedItems
+  const getClothingData = () => {
+    // console.log(user._id)
+    axios.get(`${BACKEND}/clothing/user/${user._id}`).then(
+      (res) => {
 
-                res.data.forEach((item) => {
-                  if (!tempArr.some((selectedItem) => selectedItem._id === item._id)) {
-                    tempArr.push(item);
-                  }
-                });
+        setClothings(res.data.data)
+        // console.log(clothings)
 
-                setSelectedItems(tempArr)
-            }
-        ).catch(
-            (err)=>{
+      }
+    ).catch(
+      (err) => {
+        console.log(err)
+      }
+    )
 
-            }
-        )
-    }
+  }
 
-    const forceUpdate = () => {
-        setUpdate(!update)
-    }
+  const getClosetData = () => {
+    axios.get(`${BACKEND}/clothing/closet/${closet._id}`).then(
+      (res) => {
+        // console.log(res.data)
+        // let tempArr = selectedItems;
+        // res.data.map((item,index)=>{
+        //     if(!tempArr.includes(item)){
+        //         tempArr.push(item)
+        //     }
+        // })
+        let tempArr = selectedItems.slice(); // Create a copy of selectedItems
 
-    useEffect(() => {
-        if (user !== '') {
-            setHaveData(true)
-        }
-    }, [user])
-
-    useEffect(() => {
-
-        getData()
-        getClosetData()
-
-    }, [update, haveData])
-
-    const handleAddItems = () => {
-        // console.log(selectedItems)
-        selectedItems.forEach(element => {
-            // console.log(element._id)
-            axios.put(`${BACKEND}/clothing`,{
-                clothingId : element._id,
-                closetId: closet._id
-            })
+        res.data.forEach((item) => {
+          if (!tempArr.some((selectedItem) => selectedItem._id === item._id)) {
+            tempArr.push(item);
+          }
         });
 
-        navigation.navigate("ClosetDetail", { closet });
+        setSelectedItems(tempArr)
+      }
+    ).catch(
+      (err) => {
 
+      }
+    )
+  }
 
+  const forceUpdate = () => {
+    setUpdate(!update)
+  }
+
+  useEffect(() => {
+    if (user !== '') {
+      setHaveData(true)
     }
+  }, [user])
 
-    const handleClickItem = (i) => {
-        // console.log(i.name)
-        // console.log(selectedItems)
-        let tempArr = selectedItems;
-        const index = tempArr.findIndex((selectedItem) => selectedItem._id === i._id);
+  useEffect(() => {
 
-        if (index !== -1) {
-          tempArr.splice(index, 1);
-        } else {
-          tempArr.push(i);
-        }
-        setSelectedItems(tempArr);
-        setUpdate(!update)
-        // console.log("SELECTED")
-        // console.log(selectedItems.map((item,index)=>{
-        //     console.log(item.name)
-        // }))
+    getData()
+    getClosetData()
+
+  }, [update, haveData])
+
+  const handleAddItems = () => {
+    // console.log(selectedItems)
+    selectedItems.forEach(element => {
+      // console.log(element._id)
+      axios.put(`${BACKEND}/clothing`, {
+        clothingId: element._id,
+        closetId: closet._id
+      })
+    });
+
+    navigation.navigate("ClosetDetail", { closet });
+
+
+  }
+
+  const handleClickItem = (i) => {
+    // console.log(i.name)
+    // console.log(selectedItems)
+    let tempArr = selectedItems;
+    const index = tempArr.findIndex((selectedItem) => selectedItem._id === i._id);
+
+    if (index !== -1) {
+      tempArr.splice(index, 1);
+    } else {
+      tempArr.push(i);
     }
+    setSelectedItems(tempArr);
+    setUpdate(!update)
+    // console.log("SELECTED")
+    // console.log(selectedItems.map((item,index)=>{
+    //     console.log(item.name)
+    // }))
+  }
 
-    return (
-        <>
-       <Container style={styles.container}>
-           <ScrollView>
-         {clothings.length > 0 ? (
-           clothings.map((item, index) => {
-            const isSelected = selectedItems.find((selectedItem) => selectedItem._id === item._id);
-            const fIndex = selectedItems.findIndex((selectedItem) => selectedItem._id === item._id);
+  return (
+    <>
+      <Container style={styles.container}>
+        <ScrollView>
+          {clothings.length > 0 ? (
+            clothings.map((item, index) => {
+              const isSelected = selectedItems.find((selectedItem) => selectedItem._id === item._id);
+              const fIndex = selectedItems.findIndex((selectedItem) => selectedItem._id === item._id);
 
 
-            // const isSelected = item.selected
-            // const isSelected = true
-            let display = false
-            if(isSelected !== undefined){
+              // const isSelected = item.selected
+              // const isSelected = true
+              let display = false
+              if (isSelected !== undefined) {
                 display = true
-            }
-            // console.log(selectedItems.find((selectedItem) => selectedItem._id === item._id))
-            
-          
-            return (
-              <Text
-                key={index}
-                onPress={() => handleClickItem(item)}
-                style={[styles.clothingText, fIndex!==-1 ? styles.selectedClothingText : null]}
-              >
-                {item.name}
-              </Text>
-            );
-          })
-        ) : (
-          <Text>No Clothing found</Text>
-        )}
+              }
+              // console.log(selectedItems.find((selectedItem) => selectedItem._id === item._id))
+
+
+              return (
+                <Text
+                  key={index}
+                  onPress={() => handleClickItem(item)}
+                  style={[styles.clothingText, fIndex !== -1 ? styles.selectedClothingText : null]}
+                >
+                  {item.name}
+                </Text>
+              );
+            })
+          ) : (
+            <Text>No Clothing found</Text>
+          )}
         </ScrollView>
 
         <Button title="Add Items" onPress={handleAddItems}>
           <Text>Add Items</Text>
         </Button>
-      </Container> 
-        </>
-    )
+      </Container>
+    </>
+  )
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: "1"
-      },
-      clothingText: {
-        backgroundColor: "white",
-        padding: 10,
-        marginBottom: 10
-      },
-      selectedClothingText: {
-        backgroundColor: "lightblue"
-      }
+  container: {
+    flex: 1
+  },
+  clothingText: {
+    backgroundColor: "white",
+    padding: 10,
+    marginBottom: 10
+  },
+  selectedClothingText: {
+    backgroundColor: "lightblue"
+  }
 })
